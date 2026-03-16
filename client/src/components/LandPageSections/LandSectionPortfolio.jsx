d0971f70d57eacd80f04fd1f998377a155c57ea1
Martin Quiroga <112977790+MartinQ22@users.noreply.github.com>
2026-03-16 08:44:07 -0300

    vercel fix

--- client/src/components/LandPageSections/LandSectionPortfolio.jsx
+++ client/src/components/LandPageSections/LandSectionPortfolio.jsx
@@ -1,9 +1,12 @@
+import landPortBack from "../../assets/LandPortBack.png";
+import landPortFront from "../../assets/LandPortFront.png";
+
 export default function LandSectionPortfolio() {
   return (
     <section className="portfolioSection">
       <div className="portfolioImagesContainer">
-        <img src="/src/assets/LandPortBack.png" alt="Portfolio background layer" className="portImage backLayer" />
-        <img src="/src/assets/LandPortFront.png" alt="Portfolio front layer" className="portImage frontLayer" />
+        <img src={landPortBack} alt="Portfolio background layer" className="portImage backLayer" />
+        <img src={landPortFront} alt="Portfolio front layer" className="portImage frontLayer" />
       </div>
 
       <div className="portfolioContent">
